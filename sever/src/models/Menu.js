94c06283a62af7f879326f447f8be55996a5b1c5
kychan9044 <kychan9044@naver.com>
2023-04-17 23:58:02 +0900

    DB 추가

---
+++ sever/src/models/Menu.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const db = require("../config/db");
+
+// 쿼리
+const getCategoryQuery = "SELECT * FROM Menu WHERE catergory = ?;"
+
+class Menu{
+    
+    static async getMenuByCategory(categoryId)
+    {
+        return new Promise((resolve, reject) =>{
+            const query = getCategoryQuery;
+            db.query(query, [categoryId], (err, data) => {
+                if (err) reject(err);
+                else resolve(data[0]);
+            });
+        });
+    }
+}
