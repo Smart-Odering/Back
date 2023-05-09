863964d9745716a705df92fb593c214b8ffd4797
k1mmd <likerapboy@ajou.ac.kr>
2023-05-09 19:54:02 +0900

    123

--- client/js/home.js
+++ client/js/home.js
@@ -47,7 +47,7 @@ function renderHome(categoryName) {
             $(".menu-modal-container").addClass("active");
             $(".btn-size-up").on("click",function(){
                 console.log("size up click")
-                var totalprice = price + 1000;
+                var totalprice = Number(price) + 1000;
                 document.querySelector('.modal-price').textContent = `${totalprice}원`;
             });
         });
