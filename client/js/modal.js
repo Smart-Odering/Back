65940fd5a539ef6d4f712ed170a14a5bf99c8e3b
k1mmd <likerapboy@ajou.ac.kr>
2023-05-09 22:15:37 +0900

    1212

--- client/js/modal.js
+++ client/js/modal.js
@@ -9,27 +9,27 @@ $(function(){
     $(".modal-item .btn-close").on("click",function(){
         $(".menu-modal-container").removeClass("active");
     });
-    // $(".btn-size-up").on("click",function(){
-    //     if(state == 0){
-    //         $(".btn-size-up").css("background","tomato")
-    //         $(".modal-price").css("color","orangered")
-    //         totalprice += 1000;
-    //         document.querySelector('.modal-price').textContent = `${totalprice}원`;
-    //         state = 1;
-    //     }else if(state == 1){
-    //         $(".btn-size-up").css("background","")
-    //         $(".modal-price").css("color","black")
-    //         totalprice -= 1000;
-    //         document.querySelector('.modal-price').textContent = `${totalprice}원`;
-    //         state = 0;
-    //     }
-    // });
-    // $(".btn-order").on("click",function(){
-    //     $(".modal-title").val();
-    //     console.log($(".modal-title").val())
-    //     console.log(name)
-    //     console.log(price)
-    // });
+    $(".btn-size-up").on("click",function(){
+        if(state == 0){
+            $(".btn-size-up").css("background","tomato")
+            $(".modal-price").css("color","orangered")
+            totalprice += 1000;
+            document.querySelector('.modal-price').textContent = `${totalprice}원`;
+            state = 1;
+        }else if(state == 1){
+            $(".btn-size-up").css("background","")
+            $(".modal-price").css("color","black")
+            totalprice -= 1000;
+            document.querySelector('.modal-price').textContent = `${totalprice}원`;
+            state = 0;
+        }
+    });
+    $(".btn-order").on("click",function(){
+        $(".modal-title").val();
+        console.log($(".modal-title").val())
+        console.log(name)
+        console.log(price)
+    });
 });
 
 
