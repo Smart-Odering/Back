7305057c888861ba1a607bae3e44a8754c63c246
k1mmd <likerapboy@ajou.ac.kr>
2023-05-09 20:19:26 +0900

    1212

--- client/js/home.js
+++ client/js/home.js
@@ -51,12 +51,14 @@ function renderHome(categoryName) {
                 if(state == 0){
                     console.log("size up click")
                     $(".btn-size-up").css("background","tomato")
+                    $(".modal-item").css("color","tomato")
                     totalprice += 1000;
                     document.querySelector('.modal-price').textContent = `${totalprice}원`;
                     state = 1;
                 }
                 else if(state == 1){
                     $(".btn-size-up").css("background","")
+                    $(".modal-item").css("color","black")
                     totalprice -= 1000;
                     document.querySelector('.modal-price').textContent = `${totalprice}원`;
                     state = 0;
