b621d722a9580c0fdf2cb780fe387b81a1ae9ce8
k1mmmd <likerapboy@ajou.ac.kr>
2023-05-10 22:13:41 +0900

    footer 구현

--- client/js/footer.js
+++ client/js/footer.js
@@ -1,4 +1,6 @@
 const $orderList = document.querySelector('.order-list');
+const $orderBtn = document.querySelector('.order-btn');
+const $deleteAllItems = document.querySelector('.delete-all-items');
 
 var _list = [];
 
@@ -11,15 +13,51 @@ const render = (list) => {
     console.log(list[0])
     console.log(list[1])
   
-    // _list.forEach(({menuname, totalprice }) => {
+    if (_list.length) {
+      // _list.forEach(({menuname, totalprice }) => {
       orderHtml += `<li class="order-item">
       <i class="remove-item far fa-times-circle"></i>
       <span class="item-name">${list[0]}</span>
       <span class="item-price">${list[1]}원</span>
       </li>`;
-    // });
+      // });
+      $orderList.innerHTML = orderHtml;
+    } else {
+      $orderList.innerHTML = orderHtml;
+    }
+    changeButtonColor();
+    console.log('장바구니', _list);
+};
 
-    $orderList.innerHTML = orderHtml;
+$orderList.onclick = (e) => {
+  if (!e.target.classList.contains('remove-item')) return;
+  var emptylist = [];
+  render(emptylist);
+};
+
+$deleteAllItems.onclick = () => {
+  deleteAllItems();
+};
+
+// const removeItem = (emptylist) => {
+//   model.menu = model.menu.filter((item) => item.id !== +id);
+//   render(model.menu);
+// };
+
+const changeButtonColor = () => {
+  if (_list.length) {
+    $orderBtn.classList.replace('order-btn-invalid', 'order-btn-valid');
+    $(".delete-all-items").css("background","tomato")
+  } else {
+    $orderBtn.classList.replace('order-btn-valid', 'order-btn-invalid');
+    $(".delete-all-items").css("background","#645c5c")
+  }
+};
+
+const deleteAllItems = () => {
+  if (!_list.length) return;
+  _list = [];
+  render(_list);
 };
 
 
