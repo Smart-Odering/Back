407fff122632496606f658842e21e25f55242bfd
k1mmmd <likerapboy@ajou.ac.kr>
2023-05-14 19:11:03 +0900

    음성 녹음 및 https 구현

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,10 +1,22 @@
 "use strict";
 
+const https = require("https");
+const fs = require("fs");
+
+const options = {
+    key: fs.readFileSync("./config/cert.key"),
+    cert: fs.readFileSync("./config/cert.crt"),
+};
+
 const app = require("../app");
 const logger = require("../src/config/logger");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     logger.info(`${PORT} 포트에서 서버가 가동되었습니다.`);
+});
+
+https.createServer(options, app).listen(8080, () => {
+    console.log(`HTTPS server started on port 8080`);
 });
