ab48d89d27c335c54e520a0f040d0e63425515fd
k1mmd <likerapboy@ajou.ac.kr>
2023-05-20 20:51:44 +0900

    음성인식 함수 수정 및 추천 메뉴 모달창 기초틀 제작

--- client/js/stt-modal.js
+++ client/js/stt-modal.js
@@ -3,9 +3,12 @@ const $sttRecog = document.querySelector('.stt-recog');
 const $loading = document.querySelector('.loading');
 const $sttModalDisplay = document.querySelector('.stt-modal-container');
 const $sttCancelBtn = document.querySelector('.stt-cancel-button');
+const $sttModalHeading = document.querySelector('.stt-modal-heading');
 
 var recogText = null;
 var gif = null;
+var recommendData = {};
+var recogMenuHtml = null;
 
 //음성인식 텍스트 렌더링
 const sttRender = (recogText) => {
@@ -34,10 +37,36 @@ const LoadingImage = (gif) => {
     }
 };
 
+const menuRecommend = (recommendData) => {
+    sttRender();
+    LoadingImage();
+    if (recommendData != null){
+        $sttModalHeading.textContent = "아래 메뉴를 추천합니다!";
+        recogMenuHtml =
+            `<li id="menu_${recommendData.id}" class="menu-item">
+                <figure id="menu_${recommendData.id}">
+                    <img id="menu_${recommendData.id}" class="menu-img" src="/res/${recommendData.image}">
+                    <figcaption id="menu_${recommendData.id}" class="menu-title">${recommendData.name_kor}</figcaption>
+                    <span class="menu-price">${recommendData.price}</span>
+                </figure>
+            </li>
+            ` 
+        $sttRecog.innerHTML += recogMenuHtml;
+    }
+    else{
+        $sttModalHeading.textContent = "추천 메뉴가 없습니다.";
+    }
+};
+
 // 음성인식 모달 띄우기 관련 이벤트
 $recordBtn.onclick = () => {
     $recordBtn.classList.replace('record', 'record-active');
     $sttModalDisplay.classList.replace('modal-invisible', 'modal-visible');
+    recommendData['id'] = "0";
+    recommendData['image'] = "ice_americano.jpg";
+    recommendData['name_kor'] = "아이스 아메리카노";
+    recommendData['price'] = "4500";
+    console.log(recommendData)
     // STT
     fetch("http://localhost:3001/stt")
     .then(response => response.json())
@@ -46,6 +75,7 @@ $recordBtn.onclick = () => {
         console.log(data);
         sttRender(data);
         setTimeout(LoadingImage, 1000, "./res/Spinner.gif");
+        setTimeout(menuRecommend, 2000, recommendData);
     })
     .catch(error => {
         console.error('Error:', error);
@@ -58,4 +88,5 @@ $sttCancelBtn.onclick = () => {
     $sttModalDisplay.classList.replace('modal-visible', 'modal-invisible');
     sttRender();
     LoadingImage();
+    $sttModalHeading.textContent = "음성인식 진행 중"
 };
--- python-server/server.py
+++ python-server/server.py
@@ -18,13 +18,13 @@ def stt():
         with sr.Microphone() as source:
             print('음성을 입력하세요.')
             audio = r.listen(source)
-            try:
-                recogResult = r.recognize_google(audio, language='ko-KR')
-                print('음성변환 : ' + recogResult)
-            except sr.UnknownValueError:
-                print('오디오를 이해할 수 없습니다.')
-            except sr.RequestError as e:
-                print(f'에러가 발생하였습니다. 에러원인 : {e}')
+        try:
+            recogResult = r.recognize_google(audio, language='ko-KR')
+            print('음성변환 : ' + recogResult)
+        except sr.UnknownValueError:
+            print('오디오를 이해할 수 없습니다.')
+        except sr.RequestError as e:
+            print(f'에러가 발생하였습니다. 에러원인 : {e}')
                 
     except KeyboardInterrupt:
         pass
