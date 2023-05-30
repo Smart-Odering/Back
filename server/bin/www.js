e812b07d2183894ca7c230f86985686e7c0baf3b
kychan9044 <kychan9044@naver.com>
2023-05-31 01:07:05 +0900

    predict 서버 추가

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,13 +1,5 @@
 "use strict";
 
-const https = require("https");
-const fs = require("fs");
-
-const options = {
-    key: fs.readFileSync("./config/cert.key"),
-    cert: fs.readFileSync("./config/cert.crt"),
-};
-
 const app = require("../app");
 const logger = require("../src/config/logger");
 
@@ -15,8 +7,4 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     logger.info(`${PORT} 포트에서 서버가 가동되었습니다.`);
-});
-
-https.createServer(options, app).listen(8080, () => {
-    console.log(`HTTPS server started on port 8080`);
 });
