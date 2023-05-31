1744437f36b1089dcbf0f939de5e88b9924d8c31
k1mmmd <likerapboy@ajou.ac.kr>
2023-05-31 20:58:14 +0900

    음성인식 오류 테스트

--- python-server/server.py
+++ python-server/server.py
@@ -15,7 +15,7 @@ def stt():
     try:
         r = sr.Recognizer()
         
-        with sr.Microphone(sample_rate=44100) as source:
+        with sr.Microphone(sample_rate = 48000, device_index = 2, chunk_size = 1024) as source:
             print('음성을 입력하세요.')
             audio = r.listen(source)
         try:
